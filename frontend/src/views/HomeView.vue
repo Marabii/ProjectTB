cb490011aa5f808eb0b9613af8ca2946c8d9ca9b
Marabii <minehamza97@gmail.com>
2024-12-13 19:11:09 +0100

    projetTB initial commit

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Header from '@/components/Header.vue'
+import VuePdfEmbed from 'vue-pdf-embed'
+import 'vue-pdf-embed/dist/styles/annotationLayer.css'
+import 'vue-pdf-embed/dist/styles/textLayer.css'
+
+const pdfUrl = 'https://storage.googleapis.com/projettb/1/demo_job%20ad.pdf'
+</script>
+
+<template>
+  <div class="grid">
+    <Header></Header>
+    <input
+      class="w-full my-16 justify-self-center indent-3 max-w-[900px] h-[60px] border border-secondary rounded-xl"
+      type="text"
+      placeholder="Recherchez des fiches de cours, fiches de révisions ou résumés de livres"
+    />
+
+    <VuePdfEmbed annotation-layer text-layer :source="pdfUrl" />
+  </div>
+</template>
